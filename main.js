84f802530bbc59fa7607345608c6e38280343b8e
FanilZarip <fanilfan1994@mail.ru>
2022-02-25 00:28:09 +0300

    Done

---
+++ main.js
@@ -0,0 +1,132 @@
+import {inputTask, inputButton, highListWrapper, lowListWrapper, createTaskBlock, taskList, taskStatus, tasksDeleteButton, tasksBlock} from './view.js';
+
+
+
+const UI_ELEMS_ARRAY = {
+    inputButtonArray: Array.from(inputButton),
+    inputTaskArray: Array.from(inputTask),
+}
+
+
+
+let idNumber = 0;
+
+
+UI_ELEMS_ARRAY.inputButtonArray.forEach(function (button) {
+    button.addEventListener('click', addTask);
+});
+
+
+function addTask () {
+
+    let buttonPriority = null;
+    const emptyInputMessage = 'Please enter some task';    
+    buttonPriority = this.dataset.buttonTarget;
+
+    //check that Got correct data-* 
+    console.log('click on "', buttonPriority, '" button');
+    
+    UI_ELEMS_ARRAY.inputTaskArray.forEach(createTask);
+
+    function createTask (inputData) {
+        const emptyValue = inputData.value === '';
+        const correctInput = inputData.dataset.taskPriority === buttonPriority;
+
+        if (correctInput) {
+            if (emptyValue) {
+                console.log(emptyInputMessage);
+                return;
+            }
+            else {
+                idNumber ++;
+                taskList.push({name: inputData.value, priority: buttonPriority, id: idNumber});
+                inputData.value = '';
+                createTaskBlock(buttonPriority);
+                changeStatus ();
+                deleteTask ();
+            }
+        }
+    }
+    
+    console.log(taskList);
+
+}
+
+
+function changeStatus () {
+    
+    // как получить обновленную коллекцию без передачи этой функции через addTask()?
+    const tasksBlockArray = Array.from(tasksBlock);
+
+    tasksBlockArray.forEach(changeBackground);
+
+    function changeBackground (toDoBlock) {
+        
+        let priority = toDoBlock.dataset.taskStatus;
+        const firstChildElement = toDoBlock.firstElementChild;
+        const firstChildElementChild = firstChildElement.firstElementChild;
+
+        firstChildElementChild.addEventListener('click', addClass);
+
+        function addClass () {
+            if (firstChildElementChild.checked) {
+                toDoBlock.classList.add('todo__List_checked');
+                if (priority === taskStatus.high) {
+                    highListWrapper.append(toDoBlock);
+                } else {
+                    lowListWrapper.append(toDoBlock);
+                }
+            } else {
+                    toDoBlock.classList.remove('todo__List_checked');
+                }    
+            }
+            
+    }
+}
+
+// Удаление через event.currentTarget
+// function deleteTask () {
+//     const tasksDeleteButtonArray = Array.from(tasksDeleteButton);
+//     tasksDeleteButtonArray.forEach(function(delButton){
+        // можно ли использовать addEventlistener?
+//         delButton.onclick = showButtonList;
+//     })
+    
+//     function showButtonList(event) {
+//         console.log(event.currentTarget);
+//         event.currentTarget.parentNode.remove();
+//     }
+// }
+
+// Удаление без привязки к родителю
+function deleteTask () {
+    const tasksDeleteButtonArray = Array.from(tasksDeleteButton);
+    tasksDeleteButtonArray.forEach(function(delButton){
+        delButton.onclick = getDeleteElement;
+    })
+    
+    function getDeleteElement(event) {
+        
+        const buttonId = event.currentTarget.dataset.taskId;
+        const deleteElemIndex = taskList.findIndex(function (elem){
+           return elem.id == buttonId;
+        });
+        taskList.splice(deleteElemIndex, 1);
+        
+
+        for (const block of tasksBlock) {     
+            const blockId = block.dataset.taskId
+
+            const DeletButtonIdEqualBlockId = blockId === buttonId;
+            
+            if (DeletButtonIdEqualBlockId) {
+                block.remove();
+            }
+            console.log(taskList);
+
+        } 
+    }
+}
+
+
+console.log(tasksDeleteButton);
--- view.js
+++ view.js
@@ -1,97 +1,25 @@
-const inputTask = document.querySelectorAll('.todo__taskText_input');
-const inputButton = document.querySelectorAll('.input__button');
-const highListWrapper = document.querySelector('.high__list_wrapper');
-const lowListWrapper = document.querySelector('.low__list_wrapper');
-
-console.log(inputTask);
-
-
+export const inputTask = document.querySelectorAll('.todo__taskText_input');
+export const inputButton = document.querySelectorAll('.input__button');
+export const highListWrapper = document.querySelector('.high__list_wrapper');
+export const lowListWrapper = document.querySelector('.low__list_wrapper');
+export const tasksDeleteButton = document.getElementsByClassName('task__delete_button');
+export const tasksBlock = document.getElementsByClassName('todo__List');
 
 
 
-// Move to main.js
-const taskList = [];
+export const DELETE_BUTTON_SVG = `<svg width="17" height="16" viewBox="0 0 17 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+<line y1="-0.5" x2="20.7803" y2="-0.5" transform="matrix(0.710506 0.703691 -0.65218 0.758064 1 1)" stroke="#998899"/>
+<line y1="-0.5" x2="20.8155" y2="-0.5" transform="matrix(0.693335 -0.720616 0.670126 0.742247 1.56787 16)" stroke="#998899"/>
+</svg>`;
 
-const UI_ELEMS_ARRAY = {
-    inputButtonArray: Array.from(inputButton),
-    inputTaskArray: Array.from(inputTask),
-}
-
-UI_ELEMS_ARRAY.inputButtonArray.forEach(function (button) {
-    button.addEventListener('click', addTask);
-});
-
-function changeStatus (priority) {
-    
-    const checkboxButton = document.querySelectorAll('.chckbx');
-    const tasksBlock = document.querySelectorAll('.todo__List');
-    const checkboxButtonArray = Array.from(checkboxButton);
-    const tasksBlockArray = Array.from(tasksBlock);
-
-    tasksBlockArray.forEach(function (background) {
-        const firstChildElement = background.firstElementChild;
-        const firstChildElementChild = firstChildElement.firstElementChild;
-        firstChildElementChild.addEventListener('click', changeBackground);
-        function changeBackground () {
-            if (firstChildElementChild.checked) {
-                background.classList.add('todo__List_checked');
-                if (priority === 'high') {
-                    highListWrapper.append(background);
-                } else {
-                    lowListWrapper.append(background);
-                }
-            } else {
-                    background.classList.remove('todo__List_checked');
-                }    
-            }
-            
-        })
-
-        // background.classList.add('todo__List_checked');
-    console.log(tasksBlockArray);
+export const taskList = [];
+export const taskStatus = {
+    high: 'high',
+    low: 'low',
 }
 
 
-function addTask () {
-
-    let buttonTarget = null;
-    const emptyInputMessage = 'Please enter some task';
-
-    
-    buttonTarget = this.dataset.buttonTarget;
-
-    //check that Got correct data-* 
-    console.log('click on "', buttonTarget, '" button');
-    
-    UI_ELEMS_ARRAY.inputTaskArray.forEach(function (inputData) {
-        const emptyValue = inputData.value === '';
-        const correctInput = inputData.dataset.taskPriority === buttonTarget;
-
-        if (correctInput) {
-            if (emptyValue) {
-                console.log(emptyInputMessage);
-                return;
-            }
-            else {
-                taskList.push({name: inputData.value, priority: buttonTarget});
-                inputData.value = '';
-                createTaskBlock(buttonTarget);
-                changeStatus(buttonTarget);
-            }
-        }
-    });
-
-    
-    console.log(taskList);
-
-}
-
-function createTaskBlock (priority) {
-
-    const DELETE_BUTTON_SVG = `<svg width="17" height="16" viewBox="0 0 17 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <line y1="-0.5" x2="20.7803" y2="-0.5" transform="matrix(0.710506 0.703691 -0.65218 0.758064 1 1)" stroke="#998899"/>
-    <line y1="-0.5" x2="20.8155" y2="-0.5" transform="matrix(0.693335 -0.720616 0.670126 0.742247 1.56787 16)" stroke="#998899"/>
-    </svg>`;
+export function createTaskBlock (priority) {
 
     const TASK_UI_ELEMENTS = {
         taskBlockDiv: document.createElement('div'),
@@ -100,19 +28,19 @@ function createTaskBlock (priority) {
         checkBoxSpan: document.createElement('span'),
         taskName: document.createElement('p'),
         taskDeleteButton: document.createElement('button'),
-
     }
 
-    
     TASK_UI_ELEMENTS.taskBlockDiv.classList.add('todo__List', 'd-flex');
+    TASK_UI_ELEMENTS.taskBlockDiv.setAttribute('data-task-status', priority);
+    TASK_UI_ELEMENTS.taskBlockDiv.setAttribute('data-task-id', taskList[taskList.length - 1].id);
 
-    if (priority === 'high') {
+
+
+    if (priority === taskStatus.high) {
         highListWrapper.prepend(TASK_UI_ELEMENTS.taskBlockDiv);    
     } else {
         lowListWrapper.prepend(TASK_UI_ELEMENTS.taskBlockDiv);    
     }
-
-    
 
     TASK_UI_ELEMENTS.label.classList.add("todoCheckBox");
     TASK_UI_ELEMENTS.taskBlockDiv.prepend(TASK_UI_ELEMENTS.label);
@@ -128,6 +56,7 @@ function createTaskBlock (priority) {
     TASK_UI_ELEMENTS.taskBlockDiv.append(TASK_UI_ELEMENTS.taskName);
 
     TASK_UI_ELEMENTS.taskDeleteButton.classList.add('button', 'task__delete_button');
+    TASK_UI_ELEMENTS.taskDeleteButton.setAttribute('data-task-id', taskList[taskList.length - 1].id);
     TASK_UI_ELEMENTS.taskDeleteButton.innerHTML = DELETE_BUTTON_SVG;
     TASK_UI_ELEMENTS.taskBlockDiv.append(TASK_UI_ELEMENTS.taskDeleteButton);
 }
